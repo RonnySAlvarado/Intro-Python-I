eda535666dfe3fa4c8d373a100627084539f540a
Ronny Alvarado <rsalvarado777@gmail.com>
2019-11-15 23:29:27 -0600

    Completed 11_args.py file

--- src/11_args.py
+++ src/11_args.py
@@ -6,12 +6,23 @@
 
 # YOUR CODE HERE
 
+
+def f1(x, y):
+    return x + y
+
+
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+
+def f2(*num):
+    print(num)
+    return sum(num)
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +32,21 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+
+def f3(x, y=None):
+    if y == None:
+        return x + 1
+    else:
+        return x + y
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -46,6 +65,12 @@ print(f3(8))     # Should print 9
 # Should print
 # key: a, value: 12
 # key: b, value: 30
+
+def f4(**stuff):
+    for things in stuff:
+        print(f'key: {things}, value: {stuff[things]}')
+
+
 f4(a=12, b=30)
 
 # Should print
@@ -60,4 +85,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
